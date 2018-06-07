b8488bed6484826babfb69acb7f07038c8233e9a
kmatraksa <kmatraksa1@gmail.com>
2018-06-07 19:46:05 +0700

    pretest

--- Admin/CodeBack/connectdb.php
+++ Admin/CodeBack/connectdb.php
@@ -3,9 +3,9 @@
     $host = "localhost";
     // $user = "root";
     // $pass = "";
-    $user = "h514771_birdfire";
-    $pass = "BirdFire45";
-    $db = "h514771_birdfire";
+    $user = "root";
+    $pass = "";
+    $db = "project";
 
     $connect = mysqli_connect($host,$user,$pass,$db);
     $connect ->query("set names utf8");
